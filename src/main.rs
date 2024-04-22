62c0bc108718e1fcc5a1580e8bc348c987b59a56
JohnDoe1001 <xyz793253@gmail.com>
2024-04-22 11:02:35 +0700

    Setup the main program in an ASM-esque style.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+/* 
+// @@@ PSEUDOCODE SPACE START
+
+
+
+// PSEUDOCODE SPACE END
+*/
+// @@@ LIBRARY SPACE START
+
+
+
+// LIBRARY SPACE END
+// @@@ MAIN PROGRAM SPACE START
 fn main() {
-    println!("Hello, world!");
+    let i: i32 = 0;
+    for i < 10 {
+    println!("Sanity check");
+    i += 1;
+    };
 }
+// MAIN PROGRAM SPACE END
+// @@@ FUNCTION SPACE START
+
+
+
+// FUNCTION SPACE END
